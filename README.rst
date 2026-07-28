fc05772360f663ed18c7cc7b5341e80fdea7ad30
Ziyang <baiziyang2022@yeah.net>
2026-07-28 14:41:39 +0800

    docs: migrate LSR site to Sphinx

---
+++ README.rst
@@ -0,0 +1,13 @@
+LSR 目录
+================================================================================
+
+本文档库使用 reStructuredText 与 Sphinx。
+
+构建命令：
+
+.. code-block:: sh
+
+   python -m pip install -r requirements.txt
+   python -m sphinx -b html . _build/html
+
+当前规范入口见 ``index.rst``。
---
+++ conf.py
@@ -0,0 +1,13 @@
+project = 'LSR'
+author = 'Ziyang-Bai'
+language = 'zh_CN'
+
+extensions = []
+templates_path = ['_templates']
+master_doc = 'index'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst', 'store/old/*']
+
+html_theme = 'alabaster'
+html_static_path = ['assets']
+html_favicon = 'assets/favicon.png'
+html_logo = 'assets/icon.svg'
